dc3598fb85745735377009734a5bf715f52d722b
Einfallslos03 <heiko.hahn2003@proton.me>
2026-09-23 03:04:34 +0200

    First Weapon Logic

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    publlic float bulletVelocity = 30f;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
